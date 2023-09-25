26fddd50ebda53628c6ec866635fd7ff36af3986
guideau jean <guideau221@unicaen.fr>
2023-09-25 15:36:08 +0200

    feat récuperer nom villes via API commune

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
-
-let commune = fetch('https://geo.api.gouv.fr/communes?codePostal=78000');
-
-if (commune) {
-    console.log("oui");
-}
-else {
-    console.log("help");
-}
+let commune = fetch('https://geo.api.gouv.fr/communes?codePostal=27560');
+let villes = [];
+commune
+    .then((response) => response.text())
+    .then((text) => {
+        texteSeparer = text.split(",");
+        let compteur = 0;
+        let ville;
+        texteSeparer.forEach(element => {
+            if (compteur % 8 == 0) {
+                ville = element.split("\"");
+                villes.push(ville[3]);
+            }
+            compteur++;
+        });
+    });
+console.log(villes);
