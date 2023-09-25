91986cc1a1a5e1d1f036e6c92f6e9e1dd40ed4b9
Higaara <58510205+Sirmetana@users.noreply.github.com>
2023-09-25 09:49:23 +0200

    ébauche fonction remplirChamps

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let tempMin, tempMax, probaPluie, heuresSoleil;
+let chTempMin = document.getElementById("inputTempMin");
+let chTempMax = document.getElementById("inputTempMax")
+let chProbaPluie = document.getElementById("inputProbaPluie");
+let chHeuresSoleil = document.getElementById("inputHeuresSoleil");
+
+function updateFields()
+{
+    chTempMin.textContent = tempMin + " °C";
+    chTempMax.textContent = tempMax + " °C";
+    chProbaPluie.textContent = probaPluie + " %";
+    chHeuresSoleil.textContent = heuresSoleil + "h";
+}
