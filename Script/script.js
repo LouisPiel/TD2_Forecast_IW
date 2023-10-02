8848940b0d814ae939dd150b436f76a8c7b0daff
Hugo <hugo.sefrioui@etu-unicaen.fr>
2023-10-02 17:14:13 +0200

    fix retourneComune() qui met maintenant correctement les codes insee et le nom des communes; add lors d'une selection sur la liste déroulante, changement auto sur la page

--- Script/script.js
+++ Script/script.js
@@ -3,18 +3,8 @@ let cp, ville;
 let cpChamp = document.getElementById("codeP");
 let villeChamp = document.getElementById("codeVille");
 
-//document.getElementById("boutonValider").addEventListener("click", remplirChamps);
 document.getElementById("boutonEffacer").addEventListener("click", viderChamps);
-/*
-//Rempli les champs sur appui du bouton Valider
-function remplirChamps()
-{
-    //TO DO : Récupérer les valeurs venant des APIs
-    chTempMin.textContent = tempMin + " °C";
-    chTempMax.textContent = tempMax + " °C";
-    chProbaPluie.textContent = probaPluie + " %";
-    chHeuresSoleil.textContent = heuresSoleil + "h";
-}*/
+
 
 //Vide les champs sur appui du bouton Effacer
 function viderChamps() {
@@ -24,21 +14,6 @@ function viderChamps() {
     chHeuresSoleil.textContent = "";
     //tempMin, tempMax, probaPluie, heuresSoleil = "";
 }
-// Info Météo
-const TOKEN = "9d76f6257b5f1548b921b4da509451064d8ed18f90dd162fd27827167ff4138f";
-
-let url = `https://api.meteo-concept.com/api/forecast/daily?token=${TOKEN}&insee=61100`;
-
-fetch(url).then(response =>
-    response.json().then(data => {
-        console.log(data);
-
-        document.getElementById("insee").innerHTML = data.city.name;
-        document.getElementById("pluie").innerHTML = data.forecast[0].probarain + "%";
-        document.getElementById("tmin").innerHTML = data.forecast[0].tmin;
-        document.getElementById("tmax").innerHTML = data.forecast[0].tmax;
-    })
-);
 
 //main
 
@@ -46,44 +21,47 @@ function retourneComune(cPostal) {
     let commune = fetch('https://geo.api.gouv.fr/communes?codePostal=' + cPostal);
     let listeDeroulante = document.getElementById("listeVilles");
     commune
-        .then((response) => response.text())
-        .then((text) => {
-            let texteSeparerNomCommune = text.split(",");
-            let compteur = 0;
-            let ville;
-            texteSeparerNomCommune.forEach(element => {
-                if (compteur % 8 == 0) {
-                    ville = element.split("\"");
-                    var option = document.createElement("option");
-                    let val = ville[3];
-                    option.text = val.charAt(0).toUpperCase() + val.slice(1);
-                    let insee = fetch('https://geo.api.gouv.fr/communes?nom=' + ville[3] + '&fields=departement&limit=5');
-                    insee
-                        .then((response) => response.text())
-                        .then((text) => {
-                            let texteSeparerCodeInsee = text.split(",");
-                            let compteurTemp = 0;
-                            let codeInsee;
-                            texteSeparerCodeInsee.forEach(element => {
-                                if (compteurTemp % 5 == 1) {
-                                    codeInsee = element.split("\"");
-                                    let code = codeInsee[3];
-                                    option.value = code;
-                                }
-                                compteurTemp++;
-                            });
-                        });
+        .then((response) => response.json())
+        .then((data) => {
+            if (data && data.length >= 1) {
+                data.forEach(valeur => {
+                    let option = document.createElement("option");
+                    option.textContent = valeur.nom;
+                    option.value = valeur.code;
                     listeDeroulante.appendChild(option);
-                }
-                compteur++;
-            });
+                    console.log("ville " + option.textContent + " insee " + option.value);
+                });
+            }
         });
 }
-
-
 
 function affichage() {
     retourneComune(document.getElementById("codePostalForm").value);
 }
 
 document.getElementById("codePostalForm").onchange = function () { affichage() };
+
+// Info Météo
+
+function afficherMeteo(insee) {
+
+    const TOKEN = "9d76f6257b5f1548b921b4da509451064d8ed18f90dd162fd27827167ff4138f";
+    let url = `https://api.meteo-concept.com/api/forecast/daily?token=${TOKEN}&insee=` + insee;
+
+    fetch(url).then(response =>
+        response.json().then(data => {
+            console.log(data);
+
+            document.getElementById("insee").innerHTML = data.city.name;
+            document.getElementById("pluie").innerHTML = data.forecast[0].probarain + "%";
+            document.getElementById("tmin").innerHTML = data.forecast[0].tmin;
+            document.getElementById("tmax").innerHTML = data.forecast[0].tmax;
+        })
+    );
+}
+
+document.getElementById("listeVilles").onchange = function () {
+    var selectElmt = document.getElementById("listeVilles");
+    var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
+    afficherMeteo(valeurselectionnee);
+}
